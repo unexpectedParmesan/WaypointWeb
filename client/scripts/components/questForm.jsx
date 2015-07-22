a46e6fdb5450e109bf0f8abb828315ee19845eec
joshturn <talktojosh@gmail.com>
2015-07-22 16:33:48 -0700

    (feat) add outlines to questForm and questList files

--- client/scripts/components/questForm.jsx
+++ client/scripts/components/questForm.jsx
@@ -1,1 +1,14 @@
-questForm.jsx
+var React = require('react');
+var Reflux = require('reflux');
+var Actions = require('../actions/actions');
+var User = require('../stores/user.store');
+
+var QuestForm = React.createClass({
+
+  render: function() {
+    return(
+    );
+  }
+});
+
+module.exports = QuestForm;
--- client/scripts/stores/questForm.store.jsx
+++ client/scripts/stores/questForm.store.jsx
@@ -1,1 +1,9 @@
-questForm.store.jsx
+var React = require('react');
+var Reflux = require('reflux');
+var Actions = require('../actions/actions');
+
+var questFormStore = Reflux.createStore({
+
+});
+
+module.exports = questFormStore;
