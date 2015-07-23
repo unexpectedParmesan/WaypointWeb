526530aa2527ff816b97bfa5616a39e27d8df8f7
Danielle Knudson <knudson.danielle@gmail.com>
2015-07-22 18:43:13 -0700

    (feat) Start creating api helpers for client

---
+++ client/scripts/helpers/api.helper.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // getMe()
+  // getQuests(userID) - optionally takes a user_id and returns all quests for that user, otherwise returns all quests
+  // getQuest(questID) - returns quest with specified id
+  // getWaypointsForQuest(questID) - returns waypoints for given quest id
+  // saveQuest(questObj, action) - saves a quest to database, actions should be POST/PUT
+  // saveWaypoint(waypointObj, action) - saves a waypoint to database, actions should be POST/PUT
+  // deleteQuest(questID) - deletes a quest from database
+  // deleteWaypoint(waypointID) - deletes a waypoint from database
+  // goHome() - brings the user to /home
+  // logout() - logs out the user
+};
