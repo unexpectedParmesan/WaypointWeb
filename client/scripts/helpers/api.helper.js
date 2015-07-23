fc3637d36857e89d6b06f85edaaa665d6e4b3dc2
Danielle Knudson <knudson.danielle@gmail.com>
2015-07-22 19:15:25 -0700

    (feat) getMe() will return JSON object about logged in user

--- client/scripts/helpers/api.helper.js
+++ client/scripts/helpers/api.helper.js
@@ -1,5 +1,34 @@
-module.exports = {
+var $ = require('jquery');
+
+var API = {
   // getMe()
+  getMe: function () {
+
+    return $.ajax({
+      url: 'http://localhost:3000/users/me',
+      method: 'GET',
+    })
+      .done(function () {
+        console.log('SUCCESS: ');
+        console.log(response);
+        console.log(status);
+
+        return {
+          status: status, 
+          data: response
+        };
+
+      })
+      .fail(function () {
+        console.log('ERROR: ');
+        console.log(status, errorMessage);
+        result.status = status;
+        return {
+          status: status, 
+          message: errorMessage
+        };
+      });
+  }, // end of getMe()
   // getQuests(userID) - optionally takes a user_id and returns all quests for that user, otherwise returns all quests
   // getQuest(questID) - returns quest with specified id
   // getWaypointsForQuest(questID) - returns waypoints for given quest id
@@ -10,3 +39,5 @@ module.exports = {
   // goHome() - brings the user to /home
   // logout() - logs out the user
 };
+
+module.exports = API;
