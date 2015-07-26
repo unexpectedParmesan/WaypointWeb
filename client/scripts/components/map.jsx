903f8c0c3dab94b04ec6125184bae70bd6df54db
Danielle Knudson <knudson.danielle@gmail.com>
2015-07-26 16:45:26 -0700

    (feat) Add markers for search box results and delete markers from state object when markers are deleted from map

--- client/scripts/components/map.jsx
+++ client/scripts/components/map.jsx
@@ -28,58 +28,109 @@ class WaypointMap extends React.Component {
 
     return (
       <div className='googleMap'>
+        <input type="text" ref="search" />
         <div ref="mapCanvas" style={styles.mapStyles} ></div>
       </div>
     )
   }
 
   createMap () {
+    var context = this;
 
+    // default map centers on Hack Reactor
     var mapOptions = {
       minZoom: 9,
       zoom: 16, 
       center: new GoogleMaps.LatLng(37.7837235,-122.4089778)
       
     };
+
+    // add the map to the page
     this.state.map = new GoogleMaps.Map(this.refs.mapCanvas.getDOMNode(), mapOptions);
-    var context = this;
+
+    // create the places searchBox 
+    var searchBox = new GoogleMaps.places.SearchBox(this.refs.search.getDOMNode());
+
+    // put the search box in the top left-hand corner
+    this.state.map.controls[GoogleMaps.ControlPosition.TOP_LEFT].push(this.refs.search.getDOMNode());
+    
+    // add listener to search box
+    GoogleMaps.event.addListener(searchBox, 'places_changed', function () {
+      // get the objects for the places returned by the user's search
+      var places = searchBox.getPlaces();
+      // if no places found, return
+      if (places.length === 0) { return; }
+
+      // iterate over each place and create a marker for that place
+      _.each(places, function(place) {
+        context.createMarker(place.geometry.location.A, place.geometry.location.F);
+      })
+    });
+
+    // listen for clicks on the map and add a marker for where the user clicks
     GoogleMaps.event.addListener(this.state.map, 'click', function (event) {
       context.createMarker(event.latLng.A, event.latLng.F);
     });
 
+    // if there are already waypoints set for this quest, add them to the map when the page loads
     if (this.props.waypoints) {
-      _.each(this.props.waypoints, function(obj){
-        this.createMarker(obj.latitude, obj.longitude);
+      _.each(this.props.waypoints, function(obj, index){
+        this.createMarker(obj.latitude, obj.longitude, obj.index);
       }, this)
     }
 
     return this.map;
   }
 
-  createMarker (lat, lng) {
+  createMarker (lat, lng, index) {
+      
     var context = this;
+
     var marker = new GoogleMaps.Marker({
+      index: index,
       position: new GoogleMaps.LatLng(lat, lng),
       map: this.state.map,
       animation: GoogleMaps.Animation.DROP,
+      label: this.state.count,
       draggable: true,
     });
 
+    // create an InfoWindow for each marker that displays the lat, lng for that location
     var infoBox = new GoogleMaps.InfoWindow({
       content: lat + ', ' + lng + ''
     });
 
+    // when a marker is clicked once, show the infoBox
     GoogleMaps.event.addListener(marker, 'click', function(event) {      
       infoBox.open(context.state.map, marker);
     })
 
+    // when a marker is clicked twice, remove the marker
     GoogleMaps.event.addListener(marker, 'dblclick', function(event) {
+
+      // remove marker from the map and delete the marker
       marker.setMap(null);
       marker = null;
-    })
 
-    this.state.markers.push(marker);
+      console.log(event)
+
+      _.each(context.state.markers, function(mark, index, collection) {
+        console.log(mark);
+        if ( (event.latLng.A === mark.position.A && event.latLng.F === mark.position.F) 
+         ) {
+          console.log('found marker to delete')
+          collection[index] = null;
+        }
+      }, this);
+
+      // remove the marker from this.state.markers
+      console.log(context.state.markers);
+    });
+    var markers = _.clone(this.state.markers);
+    markers.push(marker);
+    this.state.markers = markers;
     marker.setMap(this.state.map);
+
   }
 }
 
