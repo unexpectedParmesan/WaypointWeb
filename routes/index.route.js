a8f373fd512be070062699eefcedc9f31966d8a9
Danielle Knudson <knudson.danielle@gmail.com>
2015-07-26 16:45:26 -0700

    (feat) Add markers for search box results and delete markers from state object when markers are deleted from map

--- routes/index.route.js
+++ routes/index.route.js
@@ -3,7 +3,6 @@ var app         = express();
 var indexRouter = express.Router();
 
 indexRouter.get('/', function (req, res) {
-  console.log('in indexRouter');
   if (!req.session.passport.user) {
     res.redirect('/login');
   } else {
