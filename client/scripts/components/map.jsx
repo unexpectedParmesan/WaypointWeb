c469508c2a021700900a47adde43bf7a9794ceae
Colin Parsons <casparsons@gmail.com>
2015-07-27 19:24:15 -0700

    force map to remount when new quest is selected

--- client/scripts/components/map.jsx
+++ client/scripts/components/map.jsx
@@ -1,10 +1,10 @@
-/* 
+/*
 ** Map component used by Waypoint Form component
 ** Map component created with http://react-components.com/component/react-google-maps
 */
 
 var React = require('react');
-var $ = require('jquery');
+// var $ = require('jquery');
 var _ = require('underscore');
 var GoogleMaps = window.google.maps;
 
@@ -24,12 +24,6 @@ class WaypointMap extends React.Component {
     this.createMap();
   }
 
-  componentWillReceiveProps(nextProps) {
-    console.log('current markers: ', this.state.markers);
-    // this.updateMarkers(nextProps.waypoints);
-    this.createMap();
-  }
-
   render () {
 
     return (
@@ -37,21 +31,7 @@ class WaypointMap extends React.Component {
         <input type="text" ref="search" style={styles.searchInput} />
         <div ref="mapCanvas" style={styles.mapStyles} ></div>
       </div>
-    )
-  }
-
-  updateMarkers(newWaypoints) {
-    _.each(this.state.markers, function(marker) {
-      marker.setMap(null);
-    });
-    this.setState({markers: []}, () => {
-      if (this.props.waypoints) {
-        _.each(this.props.waypoints, function(obj, index){
-          // console.log('creating marker for this waypoint:', obj);
-          this.createMarker(obj.latitude, obj.longitude, obj.index);
-        }, this)
-      }   
-    });
+    );
   }
 
   createMap () {
@@ -60,22 +40,22 @@ class WaypointMap extends React.Component {
     // default map centers on Hack Reactor
     var mapOptions = {
       minZoom: 9,
-      zoom: 16, 
-      center: new GoogleMaps.LatLng(37.7837235,-122.4089778)
-      
+      zoom: 16,
+      center: new GoogleMaps.LatLng(37.7837235, -122.4089778)
+
     };
 
     // add the map to the page
     // this.state.map = new GoogleMaps.Map(this.refs.mapCanvas.getDOMNode(), mapOptions);
     var newMap = new GoogleMaps.Map(this.refs.mapCanvas.getDOMNode(), mapOptions);
 
-    // create the places searchBox 
+    // create the places searchBox
     var searchBox = new GoogleMaps.places.SearchBox(this.refs.search.getDOMNode());
 
     // put the search box in the top left-hand corner
     // this.state.map.controls[GoogleMaps.ControlPosition.TOP_LEFT].push(this.refs.search.getDOMNode());
     newMap.controls[GoogleMaps.ControlPosition.TOP_LEFT].push(this.refs.search.getDOMNode());
-    
+
     // add listener to search box
     GoogleMaps.event.addListener(searchBox, 'places_changed', function () {
       // get the objects for the places returned by the user's search
@@ -86,7 +66,7 @@ class WaypointMap extends React.Component {
       // iterate over each place and create a marker for that place
       _.each(places, function(place) {
         context.createMarker(place.geometry.location.A, place.geometry.location.F);
-      })
+      });
     });
 
     this.setState({ map: newMap }, () => {
@@ -97,16 +77,17 @@ class WaypointMap extends React.Component {
 
       // if there are already waypoints set for this quest, add them to the map when the page loads
       if (this.props.waypoints) {
-        _.each(this.props.waypoints, function(obj, index){
+        _.each(this.props.waypoints, function(obj) {
           // console.log('creating marker for this waypoint:', obj);
           this.createMarker(obj.latitude, obj.longitude, obj.index);
-        }, this)
-      } 
-    })
+        }, this);
+      }
+    });
+
   }
 
   createMarker (lat, lng, index) {
-      
+
     var context = this;
 
     var marker = new GoogleMaps.Marker({
@@ -124,9 +105,9 @@ class WaypointMap extends React.Component {
     });
 
     // when a marker is clicked once, show the infoBox
-    GoogleMaps.event.addListener(marker, 'click', function(event) {      
+    GoogleMaps.event.addListener(marker, 'click', function(event) {
       infoBox.open(context.state.map, marker);
-    })
+    });
 
     // when a marker is clicked twice, remove the marker
     GoogleMaps.event.addListener(marker, 'dblclick', function(event) {
@@ -135,13 +116,13 @@ class WaypointMap extends React.Component {
       marker.setMap(null);
       marker = null;
 
-      console.log(event)
+      console.log(event);
 
       _.each(context.state.markers, function(mark, index, collection) {
         console.log(mark);
-        if ( (event.latLng.A === mark.position.A && event.latLng.F === mark.position.F) 
+        if ( (event.latLng.A === mark.position.A && event.latLng.F === mark.position.F)
          ) {
-          console.log('found marker to delete')
+          console.log('found marker to delete');
           collection[index] = null;
         }
       }, this);
@@ -153,14 +134,14 @@ class WaypointMap extends React.Component {
     markers.push(marker);
     this.setState({markers}, () => {
       marker.setMap(this.state.map);
-    })
+    });
   }
 }
 
 var styles = {
   searchInput: {
     backgroundColor: '#ffffff',
-    height: 27, 
+    height: 27,
     fontFamily: 'Roboto',
     fontSize: 15,
     fontWeight: 300,
