29f4da14c008007a87ee0695fdc935a0f02f405e
Tamara <tmwoodson@gmail.com>
2015-07-27 17:40:14 -0700

    Start integrating map view

--- client/scripts/components/waypointForm.jsx
+++ client/scripts/components/waypointForm.jsx
@@ -6,64 +6,50 @@ var FormView = tform.form.Form;
 var Waypoint = tform.struct({
 	title: tform.Str,
 	description: tform.Str,
-  latitude: tform.Num,
-  longitude: tform.Num
+    latitude: tform.Num,
+    longitude: tform.Num
 });
 
 class WaypointForm extends React.Component {
-	// mixins: [Reflux.connect(QuestFormStore)],
 
   constructor(props) {
     super(props);
-		console.log(props);
 		this.state = {
 			waypoint: {
 				title: props.waypoint.title,
 				description: props.waypoint.description,
 				latitude: props.waypoint.latitude,
-				longitude: props.waypoint.longitude,
-				// indexInQuest: props.waypoint.indexInQuest,
+				longitude: props.waypoint.longitude
 			}
 		};
   }
 
 	componentWillReceiveProps(nextProps) {
-	    console.log('waypoint form receiving new props: ', nextProps);
 	    this.setState({
 	      waypoint: {
 	        title: nextProps.waypoint.title,
 	        description: nextProps.waypoint.description,
 	        latitude: nextProps.waypoint.latitude,
 	        longitude: nextProps.waypoint.longitude,
-	        // indexInQuest: nextProps.waypoint.indexInQuest,
 	      }
 	    });
 }
 
-	// onChange(val) {
-	// 	this.setState({
-	// 		value: val,
-	// 		// indexInQuest: 0
-	// 	});
-	// }
 
   save() {
 
-		var value = this.refs.waypointForm.getValue();
+	  var value = this.refs.waypointForm.getValue();
 
 	  if (this.state.waypoint) {
 	    var validation = this.refs.waypointForm.validate();
 	    if (validation.errors.length > 0) {
-	      console.log('Error: ', validation.errors[0].message);
 	    } else {
-				console.log('this.props.waypoint:', this.props.waypoint);
 	      var newWaypoint = {
 	        quest_id: this.props.waypoint.quest_id,
 					title: value.title,
 					description: value.description,
 					latitude: value.latitude,
 					longitude: value.longitude,
-					// index_in_quest: this.props.waypoint.index_in_quest,
 					id: this.props.waypoint.id,
 	      };
 				this.props.updateWaypoint(newWaypoint);
@@ -83,11 +69,10 @@ class WaypointForm extends React.Component {
 	    	  type={Waypoint}
 	        value={this.state.waypoint}
 	      />
-				<button onClick={this.save.bind(this)}>Save</button>
-				<button onClick={this.destroy.bind(this)}>Delete</button>
+		<button onClick={this.save.bind(this)}>Save</button>
+		<button onClick={this.destroy.bind(this)}>Delete</button>
       </div>
     );
-					// onChange={this.onChange.bind(this, this.state.waypoint)}
 	}
 
 }
