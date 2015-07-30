77db8fdf3d95cdfcebaac8a521a6119767595e93
joshturn <talktojosh@gmail.com>
2015-07-30 15:35:20 -0700

    (feat) add check for production env

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var cookieParser        = require('cookie-parser');
 var session             = require('express-session');
 var passport            = require('passport');
 var FacebookStrategy    = require('passport-facebook').Strategy;
-// var baseURL             = require('./environment');
+var environment         = require('./environment.js');
 
 // ROUTERS
 var indexRouter         = require('./routes/index.route');
@@ -36,7 +36,7 @@ app.use('/users', usersRouter);
 app.use('/logout', logoutRouter);
 
 // APP SETTINGS
-// console.log(baseURL)
+console.log(console.log(environment.ENV));
 app.set('port', process.env.PORT || 3000);
 var server = app.listen(app.get('port'), function(){
 	console.log('Server listening on port ' + app.get('port'));
