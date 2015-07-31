4e3be8b1368a4b14aff713d8c71bc06183c596e0
Colin Parsons <casparsons@gmail.com>
2015-07-30 19:18:06 -0700

    remove coords and index

--- client/scripts/components/waypointListItem.jsx
+++ client/scripts/components/waypointListItem.jsx
@@ -21,12 +21,6 @@ class WaypointListItem extends React.Component {
         <div>
           <p>{this.props.waypoint.title}</p>
           <p>{this.props.waypoint.description}</p>
-          <div>
-            <span>{this.props.waypoint.latitude}, {this.props.waypoint.longitude}</span>
-            <br />
-            <span>index in quest: {this.props.waypoint.index_in_quest}</span>
-            <br />
-          </div>
         </div>
       </div>
     );
