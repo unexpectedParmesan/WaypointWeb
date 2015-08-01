67e1efe109d29a7267bdec9f09f417cfcbb0449c
joshturn <talktojosh@gmail.com>
2015-07-31 20:03:28 -0700

    clean up code and comments

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -45,33 +45,6 @@ passport.use(new FacebookStrategy({
         newUser.save().then(function (user) {
           return done(null, user);
         });
-
-        // all new users get a template quest free of charge :)
-        // // all new users get a template quest free of charge :)
-        // newUser.save().then(function (user) {
-        //   var newQuest = new Quest({
-        //     creator_facebook_id: user.attributes.facebook_id,
-        //     title: 'Untitled Quest',
-        //     description: 'Add a description here!',
-        //     // length: '2.3 mi',
-        //     estimated_time: '2-3 hrs',
-        //   });
-        //   // all new quests get a template waypoint free of charge :)
-        //   newQuest.save().then(function(quest) {
-        //     var newWaypoint = new Waypoint({
-        //       quest_id: quest.attributes.id,
-        //       index_in_quest: 0,
-        //       latitude: 37.7852134705,
-        //       longitude: -122.4028015137,
-        //       title: 'Untitled Waypoint',
-        //       description: 'Add a description here!',
-        //     });
-        //     newWaypoint.save().then(function(waypoint) {
-        //       return done(null, user);
-        //     });
-        //   });
-
-        // });
       } else {
         return done(null, user);
       }
